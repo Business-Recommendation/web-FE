a2824389016c2a416adf2cc366e21131e43d09a9
Arvin Agas <arvagas@gmail.com>
2019-08-26 16:45:47 -0400

    footer appears on DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import AppRouter from './components/router/AppRouter'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div>
       <AppRouter />
+      <Footer />
     </div>
   )
 }
