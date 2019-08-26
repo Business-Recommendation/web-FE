88561c4f631ad4a68e004067a8b40b5fed875f4f
Arvin Agas <arvagas@gmail.com>
2019-08-26 13:52:51 -0400

    set up structure for routing

---
+++ client/src/components/router/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+const AppRouter = () => {
+    return (
+        <>
+            <Switch>
+                {/* <Route exact to='/' component={} />  */}
+                {/* <Route to='/register' component={} /> */}
+                {/* <Route to='/login' component={} /> */}
+                {/* <Route to='/listings' component={} /> */}
+                {/* <Route to='/stats' component={} /> */}
+                {/* <Route to='/update-listing' component={} /> */}
+                {/* <Route to='/add-listing' component={} /> */}
+            </Switch>
+        </>
+    )
+}
+
+export default AppRouter
