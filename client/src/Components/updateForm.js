11e11124d9427fd62eba6164e426a38fd7887372
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-08-28 14:03:53 -0400

    fixed form fields styling

--- client/src/Components/updateForm.js
+++ client/src/Components/updateForm.js
@@ -7,7 +7,7 @@ import './style.css'
 
 const UpdateForm = ({ errors, touched, values, status }) => {
   const [business, setBusiness] = useState([]);
-  console.log("this is touched", touched);
+  
 
   const states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY']
 
@@ -75,6 +75,7 @@ const FormikUpdateForm = withFormik({
   }),
 
   handleSubmit(values, { setStatus }) {
+    console.log(values)
     axios
       // values is our object with all our data on it.
       .post("https://reqres.in/api/users/", values)
